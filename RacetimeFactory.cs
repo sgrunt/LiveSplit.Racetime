dc352675eeade4467e50d1de93a81c5b3cbace02
Killklli <tvatter@hotmail.com>
2021-04-16 12:08:30 -0500

    Commit to the correct repo

--- RacetimeFactory.cs
+++ RacetimeFactory.cs
@@ -25,13 +25,13 @@ namespace LiveSplit.Racetime
         {
             return new RacetimeSettings();
         }
-        
+
         public string UpdateName => "Racetime Integration";
 
         public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.Racetime.xml";
 
         public string UpdateURL => "http://livesplit.org/update/";
 
-        public Version Version => Version.Parse("1.8.10");
+        public Version Version => Version.Parse("1.8.15");
     }
 }
